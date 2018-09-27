09bd9fd6e9beac1c53ed285ac6509c7a99e7fa6a
Ferman Güzel <fermanguzel@gmail.com>
2018-09-27 16:11:00 +0300

    ders

---
+++ scope.js
@@ -0,0 +1,18 @@
+var isim = "ferman"
+var isim = "mahsun"
+
+let sehir = "Antalya"
+sehir = "Adana"
+
+const il = ["Diyarbakır", "İzmir", "İstanbul"]
+il.push("Bursa")
+
+for (var i = 1; i < 10; i++) {
+    // console.log(i)
+}
+console.log(i)
+
+
+console.log(isim)
+console.log(sehir)
+console.log(il)
